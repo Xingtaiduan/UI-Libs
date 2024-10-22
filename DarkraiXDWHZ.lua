f48ae42598fec16bce3febf0c2c5e910021d2643
Xingtaiduan <131981549+Xingtaiduan@users.noreply.github.com>
2024-10-23 00:20:32 +0800

    Update DarkraiXDWHZ.lua

--- DarkraiXDWHZ.lua
+++ DarkraiXDWHZ.lua
@@ -97,7 +97,7 @@ function DarkraiX:Window(text,gamenme)
 
     ImageButton.Parent = ToggleGui
     ImageButton.Position = UDim2.new(0.822025776, 0, 0.0401606411, 0)
-    ImageButton.Size = UDim2.new(0, 76, 0, 70)
+    ImageButton.Size = UDim2.new(0, 70, 0, 70)
     ImageButton.Image = "rbxthumb://type=Asset&id=95848018322402&w=150&h=150"
     ImageButton.Draggable = true
     ImageButton.MouseButton1Down:connect(function()
