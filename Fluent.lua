bf7e778ef0678380654c0975df462ff23d54d04d
Xingtaiduan <131981549+Xingtaiduan@users.noreply.github.com>
2025-04-13 00:48:44 +0800

    Update Fluent.lua

--- Fluent.lua
+++ Fluent.lua
@@ -6225,7 +6225,7 @@ local InterfaceManager = {} do
 		end
 
 		table.insert(paths, self.Folder)
-		table.insert(paths, self.Folder .. "/")
+		table.insert(paths, self.Folder .. "/settings")
 
 		for i = 1, #paths do
 			local str = paths[i]
